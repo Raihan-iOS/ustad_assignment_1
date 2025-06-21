4879286ead039a6a7e538ccfc0ec0482f33bf25b
Raihan-iOS <md.fasi.raihan.ssl@gmail.com>
2025-06-22 01:24:17 +0600

    assignemnt 1 completed

--- assignment_1/bin/assignment_1.dart
+++ assignment_1/bin/assignment_1.dart
@@ -1,5 +1,23 @@
-import 'package:assignment_1/assignment_1.dart' as assignment_1;
+abstract class Vehicle {
+  int _speed = 0;
+  void move();
+
+  set speed(int speedValue) {
+    _speed = speedValue;
+  }
+
+  get speedValue => _speed;
+}
+
+class Car extends Vehicle {
+  @override
+  void move() {
+    print('Car is moving at $speedValue km/h');
+  }
+}
 
 void main() {
-  print('Hello world: ${assignment_1.calculate()}!');
+  Car car = Car();
+  car.speed = 80;
+  car.move();
 }
